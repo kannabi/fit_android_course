98436d53455c89740baf3b42341d3554b7765014
kannabi <v.shchukin@skyeng.ru>
2021-11-07 19:49:50 +0700

    add onSavedInstancwState

--- app/src/main/java/com/example/android_course/aac/AacActivity.kt
+++ app/src/main/java/com/example/android_course/aac/AacActivity.kt
@@ -1,8 +1,8 @@
 package com.example.android_course.aac
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android_course.databinding.ActivityAacBinding
 
 class AacActivity : AppCompatActivity() {
@@ -16,10 +16,21 @@ class AacActivity : AppCompatActivity() {
         binding = ActivityAacBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
         binding.title.text = "LOL KEK TITLE"
-        sphericalPermanentConnectionListener = SphericalPermanentConnectionListener(lifecycle = lifecycle) {
+        sphericalPermanentConnectionListener = SphericalPermanentConnectionListener(
+            lifecycle = lifecycle,
+            registryOwner = this
+        ) {
             // do something
         }
 
         lifecycle.addObserver(sphericalPermanentConnectionListener)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
     }
 }
--- app/src/main/java/com/example/android_course/aac/SphericalPermanentConnectionListener.kt
+++ app/src/main/java/com/example/android_course/aac/SphericalPermanentConnectionListener.kt
@@ -1,15 +1,23 @@
 package com.example.android_course.aac
 
+import android.os.Bundle
 import android.util.Log
+import androidx.core.os.bundleOf
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryOwner
 
 class SphericalPermanentConnectionListener(
     private val service: SphericalPermanentConnectionService = SphericalPermanentConnectionService(),
     private val lifecycle: Lifecycle,
+    private val registryOwner: SavedStateRegistryOwner,
     private val onUpdate: (SphericalData) -> Unit
-) : DefaultLifecycleObserver {
+) : DefaultLifecycleObserver, SavedStateRegistry.SavedStateProvider {
+
+    private var sphericalId: Int = -1
 
     init {
         service.registerListener {
@@ -17,6 +25,18 @@ class SphericalPermanentConnectionListener(
                 onUpdate(it)
             }
         }
+
+        registryOwner.lifecycle.addObserver(LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_CREATE) {
+                val savedStateRegistry = registryOwner.savedStateRegistry
+                savedStateRegistry.registerSavedStateProvider(
+                    PERMANENT_CONNECTION_SAVED_STATE_KEY, this
+                )
+                savedStateRegistry.consumeRestoredStateForKey(PERMANENT_CONNECTION_SAVED_STATE_KEY)
+                    ?.getInt(SPHERICAL_ID_KEY)
+                    ?.let(::sphericalId::set)
+            }
+        })
     }
 
     override fun onStart(owner: LifecycleOwner) {
@@ -29,5 +49,16 @@ class SphericalPermanentConnectionListener(
         super.onStop(owner)
         Log.d("SPCL", "onStop")
         service.disconnect()
+    }
+
+    override fun saveState(): Bundle =
+//        bundleOf(SPHERICAL_ID_KEY to sphericalId)
+        bundleOf(SPHERICAL_ID_KEY to 0)
+
+    companion object {
+        private const val PERMANENT_CONNECTION_SAVED_STATE_KEY =
+            "PERMANENT_CONNECTION_SAVED_STATE_KEY"
+
+        private const val SPHERICAL_ID_KEY = "SPHERICAL_ID_KEY"
     }
 }
